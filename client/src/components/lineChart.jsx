137ca785091df82dd6287a15e94790620b7028c6
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-07 14:13:55 -0500

    implemented hooks, request to api successful

--- client/src/components/lineChart.jsx
+++ client/src/components/lineChart.jsx
@@ -1,6 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const LineChart = () => {
+
+    const [historicalData, setHistoricalData] = useState();
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const res = await axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=30')
+                setHistoricalData(res.data);
+                console.log(res.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getData();
+    }, [])
+
     return (
         <div>
             <h1> Line Chart </h1>
