fc6554c16cdef16b8a45e32fa2ba4309870e5422
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-07 14:01:10 -0500

    established linechart.jsx as functional component

---
+++ client/src/components/lineChart.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const LineChart = () => {
+    return (
+        <div>
+            <h1> Line Chart </h1>
+        </div>
+    );
+}
+
+export default LineChart;
