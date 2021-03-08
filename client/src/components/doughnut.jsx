c4838cd2b51e1546aaab0d68450e92061ee74142
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-08 14:50:44 -0500

    implemented useEffect hook with asynchronous function

--- client/src/components/doughnut.jsx
+++ client/src/components/doughnut.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 
@@ -5,7 +6,17 @@ const Doughnut = () => {
 
     const [currentData, setcurrentData] = useState([]);
 
-
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get('https://disease.sh/v3/covid-19/all')
+                setcurrentData(res.data)
+                console.log(currentData);
+            } catch (e) {
+                console.error(e)
+            }
+        }
+    })
 
     return (
         <div>
