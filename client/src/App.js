c9014b1ce915bfd020b3fe403f76f918fb9e49af
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-08 14:53:14 -0500

    Doughnut comp rendered to dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Doughnut from './components/doughnut';
 import LineChart from './components/lineChart';
 
 function App() {
   return (
     <div>
+      <Doughnut />
       <LineChart />
     </div>
   );
