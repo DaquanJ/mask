d981fecfb27265e1b224cc6b1cb06c78ac304527
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-08 14:43:36 -0500

    established doughnut.jsx as functional component, imported hooks

---
+++ client/src/components/doughnut.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Doughnut = () => {
+    return (
+        <div>
+            <h1> Doughnut Chart </h1>
+        </div>
+    );
+}
+
+export default Doughnut;
