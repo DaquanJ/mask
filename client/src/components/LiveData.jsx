9f694a236545bb1366752dbc33be9b040b03a1f7
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-09 14:08:27 -0500

    established state with useState hook, created async function within useEffect hook, established connection to api

--- client/src/components/LiveData.jsx
+++ client/src/components/LiveData.jsx
@@ -1,7 +1,24 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 
 const LiveData = () => {
+
+    const [liveData, setLiveData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get('https://disease.sh/v3/covid-19/all')
+                setLiveData(res.data)
+                console.log(res.data)
+            } catch (e) {
+                console.error(e)
+            }
+        }
+        fetchData();
+    }, [])
+
     return (
         <div>
             <h1> Live ! </h1>
