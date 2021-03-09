26aeec9f363b9591071202dad541da66a5babad4
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-09 14:37:28 -0500

    all data rendered

--- client/src/components/LiveData.jsx
+++ client/src/components/LiveData.jsx
@@ -4,14 +4,14 @@ import React, { useState, useEffect } from 'react';
 
 const LiveData = () => {
 
-    const [liveData, setLiveData] = useState([]);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const res = await axios.get('https://disease.sh/v3/covid-19/all')
-                setLiveData(res.data)
-                console.log(res.data)
+                setData([res.data])
+                console.log([res.data])
             } catch (e) {
                 console.error(e)
             }
@@ -22,8 +22,30 @@ const LiveData = () => {
     return (
         <div>
             <h1> Live ! </h1>
+            {
+                data.map((info) => (
+                    <div className='cards' >
+                        <div>
+                            <p> Cases </p>
+                            <h1> + {info.todayCases} </h1>
+                            <p> {info.cases} Total </p>
+                        </div>
+                        <div>
+                            <p> Deaths </p>
+                            <h1> + {info.todayDeaths} </h1>
+                            <p> {info.deaths} Total </p>
+                        </div>
+                        <div>
+                            <p> Recovered </p>
+                            <h1> + {info.todayRecovered} </h1>
+                            <p> {info.recovered} Total </p>
+                        </div>
+                    </div>
+
+                ))
+            }
         </div>
-    );
+    )
 }
 
 export default LiveData;
