91959fa90558271fc69f4e6e31902aa22f8913be
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-09 14:03:36 -0500

    rendered liveData comp in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 import LineChart from './components/lineChart';
+import LiveData from './components/LiveData';
 
 function App() {
   return (
     <div>
-
+      <LiveData />
       <LineChart />
     </div>
   );
