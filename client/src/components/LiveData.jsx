78d81a36b3d0f998a48109be9652bf626d9b9e54
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-09 14:02:58 -0500

    established liveData file as functional component

---
+++ client/src/components/LiveData.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+
+const LiveData = () => {
+    return (
+        <div>
+            <h1> Live ! </h1>
+        </div>
+    );
+}
+
+export default LiveData;
