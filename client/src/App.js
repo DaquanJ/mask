3d1adf3b904fae9e0c8e4ed902a5529e81201faa
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-13 15:20:00 -0500

    implemented route

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,18 @@ import LineChart from './components/lineChart';
 import LiveData from './components/LiveData';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Country from './components/country';
 
 function App() {
   return (
     <div className='App'>
-      <h1> Mask </h1>
-      <LiveData />
-      <Countries />
-      <LineChart />
+      <Router>
+        <Route path='/:country' component={Country} />
+        <h1> Mask </h1>
+        <LiveData />
+        <Countries />
+        <LineChart />
+      </Router>
     </div>
   );
 }
