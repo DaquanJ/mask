c6ee24615ecdf234517e4b436e85316ff6711efa
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-13 15:12:06 -0500

    established connected to api via countries endpoint

--- client/src/components/countries.jsx
+++ client/src/components/countries.jsx
@@ -1,6 +1,24 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Countries = () => {
+
+    const [countries, setCountries] = useState([]);
+
+    async function getCountries() {
+        try {
+            const res = await axios.get('https://disease.sh/v3/covid-19/countries')
+            setCountries(res.data)
+            console.log(res.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getCountries()
+    }, [])
+
     return (
         <div>
             <select name="" id="">
