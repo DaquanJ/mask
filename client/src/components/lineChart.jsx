04c8bbf1880e26cbe2224bf9bb157644a9419da8
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-13 13:15:21 -0500

    added handleChange to accept user input, added handleSubmit function to make request with input, implemented form with input field to accept lastdays to view

--- client/src/components/lineChart.jsx
+++ client/src/components/lineChart.jsx
@@ -7,6 +7,8 @@ import '../styles/lineChart.css'
 const LineChart = () => {
 
     const [historicalData, setHistoricalData] = useState([]);
+    const [days, setDays] = useState({});
+
 
     function modifiedData(data) {
         let chart = [];
@@ -24,11 +26,9 @@ const LineChart = () => {
             }
             point = data
         }
-        console.log(chart)
+
         return chart;
     }
-
-
 
     useEffect(() => {
         async function getData() {
@@ -36,6 +36,7 @@ const LineChart = () => {
                 const res = await axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=365')
                 const newChart = modifiedData(res.data)
                 setHistoricalData(newChart);
+                console.log(res.data)
             } catch (error) {
                 console.error(error)
             }
@@ -43,57 +44,81 @@ const LineChart = () => {
         getData();
     }, [])
 
+    function handleChange(e) {
+        const { id, value } = e.target;
+        setDays({ [id]: value })
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        try {
+            const res = await axios.get(`https://disease.sh/v3/covid-19/historical/all?lastdays=${days.lastDays}`)
+            const newChart = modifiedData(res.data)
+            setHistoricalData(newChart);
+            console.log(res.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
 
     return (
-        <div className='chart' >
-            {historicalData.length > 0 &&
+        <div>
+            <form onChange={(e) => handleChange(e)} onSubmit={(event) => handleSubmit(event)} >
+                <input type="text" id='lastDays' placeholder='Choose Days to view' />
+                <input type="submit" />
+            </form>
+            <div className='chart' >
+                {historicalData.length > 0 &&
 
-                <Line
-                    data={{
-                        labels: historicalData.map(({ date }) => date),
-                        datasets: [
-                            {
-                                label: 'Cases',
-                                data: historicalData.map(({ cases }) => cases),
-                                borderColor: '#3333ff',
-                                fill: true,
-                            },
-                            {
-                                label: 'Deaths',
-                                data: historicalData.map(({ deaths }) => deaths),
-                                fill: true,
-                                backgroundColor: 'rgba(255,0,0,0.5)',
-                                borderColor: 'red',
-                            },
-                            {
-                                label: 'Recovered',
-                                data: historicalData.map(({ recovered }) => recovered),
-                                fill: true,
-                                backgroundColor: 'rgb(0,191,255, 0.5)',
-                                borderColor: 'rgb(65,105,225)',
-                            },
-
-                        ],
-                    }}
-
-                    options={{
-                        scales: {
-                            xAxes: [
+                    <Line
+                        data={{
+                            labels: historicalData.map(({ date }) => date),
+                            datasets: [
                                 {
-                                    type: "time",
-                                    time: {
-                                        parser: "MM/DD/YY",
-                                        tooltipFormat: "ll",
-                                    },
+                                    label: 'Cases',
+                                    data: historicalData.map(({ cases }) => cases),
+                                    borderColor: '#3333ff',
+                                    fill: true,
                                 },
-                            ]
-                        }
-                    }}
-                />
-            }
+                                {
+                                    label: 'Deaths',
+                                    data: historicalData.map(({ deaths }) => deaths),
+                                    fill: true,
+                                    backgroundColor: 'rgba(255,0,0,0.5)',
+                                    borderColor: 'red',
+                                },
+                                {
+                                    label: 'Recovered',
+                                    data: historicalData.map(({ recovered }) => recovered),
+                                    fill: true,
+                                    backgroundColor: 'rgb(0,191,255, 0.5)',
+                                    borderColor: 'rgb(65,105,225)',
+                                },
+
+                            ],
+                        }}
+
+                        options={{
+                            scales: {
+                                xAxes: [
+                                    {
+                                        type: "time",
+                                        time: {
+                                            parser: "MM/DD/YY",
+                                            tooltipFormat: "ll",
+                                        },
+                                    },
+                                ]
+                            }
+                        }}
+                    />
+                }
+
+            </div>
 
         </div>
-
     );
 }
 
