3469a48fe13c80e0c258a732dc5f7c6eb58cb183
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-16 15:42:38 -0400

    rendered countries comp, moved h1 mask element

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ChartData from './ChartData';
+import Countries from './countries';
 import LiveData from './LiveData';
 
 const Home = () => {
     return (
         <div>
+            <h1> Mask </h1>
+            <Countries />
             <LiveData />
             <ChartData />
         </div>
