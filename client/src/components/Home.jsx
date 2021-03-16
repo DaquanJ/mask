c98d91f77d7827b9cde0e3dd54048a59e4f2e812
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-16 15:35:00 -0400

    rendered liveData and ChartData comps in home comp

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <LiveData />
+            <LineChart />
+        </div>
+    );
+}
+
+export default Home;
