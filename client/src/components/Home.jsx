da07577f83a4ef36342b98d0f41b5138d8f2d4ef
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-16 17:40:38 -0400

    some things were moved around

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,16 +3,11 @@ import React from 'react';
 import '../styles/Home.css';
 
 import ChartData from './ChartData';
-import Countries from './countries';
 import LiveData from './LiveData';
 
 const Home = () => {
     return (
         <div className='Home'>
-            <div className='header'>
-                <h1> Mask </h1>
-                <Countries />
-            </div>
             <LiveData />
             <ChartData />
         </div>
