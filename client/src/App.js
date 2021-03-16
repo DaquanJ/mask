6f0b6674f6d590f480ae14f1280ca70464b53894
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-16 15:39:41 -0400

    added route for home component rendering ChartData and LiveData, removed components from being individually rendered to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import Countries from './components/countries';
 
-import LineChart from './components/lineChart';
+import LineChart from './components/ChartData';
 import LiveData from './components/LiveData';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Country from './components/country';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <Switch>
-          <Route path='/:country' exact component={Country} />
+          <Route path='/' exact component={Home} />
+          <Route path='/:country' component={Country} />
           <div>
             <div className='header'>
               <h1> Mask </h1>
               <Countries />
             </div>
-            <LiveData />
-            <LineChart />
           </div>
         </Switch>
       </Router>
