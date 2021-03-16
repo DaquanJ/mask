dfa71f0a698968bfc832c022c444fb51fc280920
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-16 15:49:29 -0400

    added container for header elements, added classNames and ids

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+
+import '../styles/Home.css';
+
 import ChartData from './ChartData';
 import Countries from './countries';
 import LiveData from './LiveData';
 
 const Home = () => {
     return (
-        <div>
-            <h1> Mask </h1>
-            <Countries />
+        <div className='Home'>
+            <div className='header'>
+                <h1> Mask </h1>
+                <Countries />
+            </div>
             <LiveData />
             <ChartData />
         </div>
