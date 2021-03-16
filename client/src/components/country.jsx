05d6508433d68e9e497afde68306afd1dd1ba68f
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-16 16:03:59 -0400

    established connection to live data by country

--- client/src/components/country.jsx
+++ client/src/components/country.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Country = ({ match }) => {
@@ -6,9 +7,20 @@ const Country = ({ match }) => {
     const [chartData, setChartData] = useState([])
 
 
+    async function getLiveData() {
+        try {
+            const res = await axios.get('https://disease.sh/v3/covid-19/countries/uk?strict=true')
+            setLiveData(res.data)
+            console.log(res.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
 
     useEffect(() => {
-
+        getLiveData();
     }, [])
 
     return (
